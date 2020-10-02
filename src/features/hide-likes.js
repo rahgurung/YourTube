8c2828a35f2295f571d5f990314d6fb48d1fdda1
brainshaker <parupp@gmx.de>
2020-10-02 23:47:24 +0200

    fix(hide-likes): fixes likes showing in tooltip on hover

--- src/features/hide-likes.js
+++ src/features/hide-likes.js
@@ -3,4 +3,7 @@ import elementReady from 'element-ready';
 export default async function () {
     await elementReady('yt-formatted-string.style-scope.ytd-toggle-button-renderer.style-text');
     $('yt-formatted-string.style-scope.ytd-toggle-button-renderer.style-text').hide();
+
+    await elementReady('paper-tooltip.style-scope.ytd-sentiment-bar-renderer');
+    $('paper-tooltip.style-scope.ytd-sentiment-bar-renderer').hide();
 }
