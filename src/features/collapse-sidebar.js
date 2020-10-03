680454f0b77bc573426c840ee012bb2d93ffc3aa
R. Gurung <gurungrahul2@gmail.com>
2020-10-04 00:39:12 +0530

    add `feature-hide-autoplay` (#29)

--- src/features/collapse-sidebar.js
+++ src/features/collapse-sidebar.js
@@ -2,7 +2,6 @@ import elementReady from 'element-ready';
 
 export default async function () {
     await elementReady('yt-icon-button#guide-button.style-scope.ytd-masthead');
-    console.log($('yt-icon-button#guide-button.style-scope.ytd-masthead button').attr('aria-pressed'));
     if ($('yt-icon-button#guide-button.style-scope.ytd-masthead button').attr('aria-pressed') === 'true') {
         $('yt-icon-button#guide-button.style-scope.ytd-masthead').trigger('click');
     }
