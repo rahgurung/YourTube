895250fc8fe26f1c39836755958492efabed266b
iamsahil1910 <iamsahil1910@gmail.com>
2020-10-03 12:26:15 +0530

    removing bottom line from description

--- src/features/hide-comments.js
+++ src/features/hide-comments.js
@@ -2,5 +2,10 @@ import elementReady from 'element-ready';
 
 export default async function () {
     await elementReady('ytd-comments#comments');
+    await elementReady('ytd-video-secondary-info-renderer');
+    // var descriptionSection = document.querySelector('.ytd-video-secondary-info-renderer');
+    // console.log(descriptionSection);
     $('ytd-comments#comments').hide();
+    $('ytd-video-secondary-info-renderer').style.borderBottom = 0;
+   
 }
