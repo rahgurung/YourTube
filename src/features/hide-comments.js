e8847413a8f4d29b3b9453cb58d068af45f1a09c
iamsahil1910 <iamsahil1910@gmail.com>
2020-10-03 12:47:01 +0530

    error fixed

--- src/features/hide-comments.js
+++ src/features/hide-comments.js
@@ -4,5 +4,5 @@ export default async function () {
     await elementReady('ytd-comments#comments');
     await elementReady('ytd-video-secondary-info-renderer');
     $('ytd-comments#comments').hide();
-    $('ytd-video-secondary-info-renderer').css("border-bottom", 0);
+    $('ytd-video-secondary-info-renderer').css('border-bottom', 0);
 }
