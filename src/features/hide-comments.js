bdcf8757d320adf288d53837d026a0c611ddeb7a
Sahil verma <42215406+iamsahil1910@users.noreply.github.com>
2020-10-03 12:49:15 +0530

    add `feature-hide-comments` (#17)
    Co-authored-by: Rahul Gurung <gurungrahul2@gmail.com>

---
+++ src/features/hide-comments.js
@@ -0,0 +1,8 @@
+import elementReady from 'element-ready';
+
+export default async function () {
+    await elementReady('ytd-comments#comments');
+    await elementReady('ytd-video-secondary-info-renderer');
+    $('ytd-comments#comments').hide();
+    $('ytd-video-secondary-info-renderer').css('border-bottom', 0);
+}
