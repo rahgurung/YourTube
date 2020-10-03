9f86c5ea4258fc305838f14adfa0a45870f846a3
Rahul Gurung <gurungrahul2@gmail.com>
2020-10-03 12:09:42 +0530

    hide the likes indicator

--- src/features/hide-likes.js
+++ src/features/hide-likes.js
@@ -2,5 +2,7 @@ import elementReady from 'element-ready';
 
 export default async function () {
     await elementReady('yt-formatted-string.style-scope.ytd-toggle-button-renderer.style-text');
+    await elementReady('ytd-sentiment-bar-renderer.style-scope.ytd-video-primary-info-renderer');
     $('yt-formatted-string.style-scope.ytd-toggle-button-renderer.style-text').hide();
+    $('ytd-sentiment-bar-renderer.style-scope.ytd-video-primary-info-renderer').hide();
 }
