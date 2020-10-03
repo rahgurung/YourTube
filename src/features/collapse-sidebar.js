7ce1cd7216b55e95e9316093058cec13b36bd2e0
R. Gurung <gurungrahul2@gmail.com>
2020-10-03 21:33:18 +0530

    collapse only if uncollapsed (#28)

--- src/features/collapse-sidebar.js
+++ src/features/collapse-sidebar.js
@@ -2,5 +2,8 @@ import elementReady from 'element-ready';
 
 export default async function () {
     await elementReady('yt-icon-button#guide-button.style-scope.ytd-masthead');
-    $('yt-icon-button#guide-button.style-scope.ytd-masthead').trigger('click');
+    console.log($('yt-icon-button#guide-button.style-scope.ytd-masthead button').attr('aria-pressed'));
+    if ($('yt-icon-button#guide-button.style-scope.ytd-masthead button').attr('aria-pressed') === 'true') {
+        $('yt-icon-button#guide-button.style-scope.ytd-masthead').trigger('click');
+    }
 }
