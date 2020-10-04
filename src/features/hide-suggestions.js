49f0e055bf3d3262c2ee4916c95b0fac21a16951
iamsahil1910 <iamsahil1910@gmail.com>
2020-10-04 06:34:26 +0530

    feature-add-sugestions\

--- src/features/hide-suggestions.js
+++ src/features/hide-suggestions.js
@@ -1,7 +1,6 @@
 import elementReady from 'element-ready';
 
 export default async function () {
-  
     await elementReady('div.style-scope ytd-watch-next-secondary-results-renderer');
     $('div.style-scope ytd-watch-next-secondary-results-renderer').hide();
 }
