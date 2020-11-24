a48c9e0e3a095dfc30f7a5c2bb791b3caef05a23
R. Gurung <gurungrahul2@gmail.com>
2020-11-24 13:21:47 +0530

    fixed the `feature-hide-suggestions` hiding whole sidebar (#41)

--- src/features/hide-suggestions.js
+++ src/features/hide-suggestions.js
@@ -1,6 +1,11 @@
 import elementReady from 'element-ready';
 
 export default async function () {
-    await elementReady('div#secondary.style-scope.ytd-watch-flexy');
-    $('div#secondary.style-scope.ytd-watch-flexy').hide();
+    await elementReady('ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer');
+    await elementReady('ytd-compact-radio-renderer.style-scope.ytd-watch-next-secondary-results-renderer');
+    await elementReady('div#continuations.style-scope.ytd-watch-next-secondary-results-renderer');
+
+    $('ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer').hide();
+    $('ytd-compact-radio-renderer.style-scope.ytd-watch-next-secondary-results-renderer').hide();
+    $('div#continuations.style-scope.ytd-watch-next-secondary-results-renderer').hide();
 }
