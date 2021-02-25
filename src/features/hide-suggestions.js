f7b1d232dc31fd1aedd1387a4d1f4138a1a8e39c
Rahul Gurung <gurungrahul2@gmail.com>
2021-02-25 22:16:57 +0530

    fixed the hide recommendation not working

--- src/features/hide-suggestions.js
+++ src/features/hide-suggestions.js
@@ -1,11 +1,22 @@
 import elementReady from 'element-ready';
 
 export default async function () {
-    await elementReady('ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer');
-    await elementReady('ytd-compact-radio-renderer.style-scope.ytd-watch-next-secondary-results-renderer');
-    await elementReady('div#continuations.style-scope.ytd-watch-next-secondary-results-renderer');
 
-    $('ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer').hide();
-    $('ytd-compact-radio-renderer.style-scope.ytd-watch-next-secondary-results-renderer').hide();
-    $('div#continuations.style-scope.ytd-watch-next-secondary-results-renderer').hide();
+    // Let 'loader' be ready
+    await elementReady('ytd-continuation-item-renderer.style-scope.ytd-item-section-renderer');
+    // Let video recommendations be ready
+    await elementReady('ytd-compact-video-renderer.style-scope.ytd-item-section-renderer');
+    // Let related chip bar be ready
+    await elementReady('yt-related-chip-cloud-renderer.style-scope.ytd-watch-next-secondary-results-renderer');
+
+    // We don't need to wait for all of them because that will make it break in pages where it is
+    // not ready
+    // We will only wait for elementReady on common elements which is there in all videos like
+    // simple video recommendations
+
+    // Hide them
+    $('ytd-continuation-item-renderer.style-scope.ytd-item-section-renderer').hide();
+    $('ytd-compact-video-renderer.style-scope.ytd-item-section-renderer').hide();
+    $('ytd-compact-movie-renderer.style-scope.ytd-item-section-renderer').hide();
+    $('yt-related-chip-cloud-renderer.style-scope.ytd-watch-next-secondary-results-renderer').hide();
 }
